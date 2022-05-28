d83ce90d9c1bcdcdef17c387970293a933fcf908
Julio Casado castillo <f1r3544@MacBook-Pro-Mac.local>
2022-05-28 15:40:56 +0200

    UI and event handler improved, now key enters sends msg

--- public/chat.js
+++ public/chat.js
@@ -6,13 +6,25 @@ text_area = document.getElementById('text-area');
 //making socket instance
 var socket = io.connect('');
 
+// add event when pressing enter key
+text_msg.addEventListener("keypress", (e) => {
+    if(e.key === 'Enter')
+    // then sending msg
+    btn_send.click();
+});
+
 // capturing events from the DOM
 btn_send.addEventListener('click', (e) =>{
     //sending events to socket with data
+    // checking first is there is content to send
+    if(text_msg.value !== '')
+
     socket.emit('chat', {
         user: text_user.value,
         msg: text_msg.value
     });
+    // cleaning msg textbox
+    text_msg.value = '';
 });
 
 socket.on('chat', (data) =>{
